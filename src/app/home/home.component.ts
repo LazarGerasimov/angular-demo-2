433147bd4d87f9de4b09d7d4cff6fa0e9f281396
Laz <lazgerasimov@gmail.com>
2022-12-03 16:23:24 +0000

    adding Todo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Todo {
+  title: string;
+  isCompleted: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  constructor() {
+  todos: Todo[] = [
+    {
+      title: 'Todo 1', isCompleted: false
+    },
+    {
+      title: 'Todo 2', isCompleted: false
+    },
+    {
+      title: 'Todo 3', isCompleted: false
+    },
+    {
+      title: 'Todo 4', isCompleted: false
+    },
+    
+  ]
 
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 }
